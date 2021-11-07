f6f7e007747bd25faaa83cd28eb5b2409d32a623
Valery Efremov <valeriy131100@gmail.com>
2021-11-07 21:52:38 +0300

    Extract image download to function

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import requests
 import os
 from pathlib import Path
 
+
+def download_image(image_url, image_path):
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    image_dirname = os.path.dirname(image_path)
+    Path(image_dirname).mkdir(parents=True, exist_ok=True)
+
+    with open(image_path, 'wb') as file:
+        file.write(response.content)
+
+
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 filename = 'hubble.jpeg'
 file_path = 'images'
 
-Path(file_path).mkdir(parents=True, exist_ok=True)
-
-response = requests.get(url)
-response.raise_for_status()
-
-with open(os.path.join(file_path, filename), 'wb') as file:
-    file.write(response.content)
+download_image(url, os.path.join(file_path, filename))
