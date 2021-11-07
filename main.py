0cf8e8014371223fe73b5d46faaa76624fe7aa34
Valery Efremov <valeriy131100@gmail.com>
2021-11-07 22:24:06 +0300

    Extract spacex images downloading to function

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ def download_image(image_url, image_path):
         file.write(response.content)
 
 
-launches_url = 'https://api.spacexdata.com/v4/launches'
-response = requests.get(launches_url)
-response.raise_for_status()
-launches = response.json()
+def fetch_spacex_last_launch():
+    launches_url = 'https://api.spacexdata.com/v4/launches'
+    response = requests.get(launches_url)
+    response.raise_for_status()
+    launches = response.json()
 
-images = []
-for launch in launches:
-    images = launch['links']['flickr']['original']
-    if images:
-        for image_num, image_link in enumerate(images):
-            image_name = f'spacex{image_num}.jpg'
-            image_path = os.path.join('images', image_name)
-            download_image(image_link, image_path)
-        break
+    for launch in launches:
+        images = launch['links']['flickr']['original']
+        if images:
+            for image_num, image_link in enumerate(images):
+                image_name = f'spacex{image_num}.jpg'
+                image_path = os.path.join('images', image_name)
+                download_image(image_link, image_path)
+            return
+
+
+fetch_spacex_last_launch()
