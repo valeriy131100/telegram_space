edb08a97679e885ab5e7ce7b6db5943acd267a13
Valery Efremov <valeriy131100@gmail.com>
2021-11-07 21:37:50 +0300

    Add image downloading

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+filename = 'hubble.jpeg'
+
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
