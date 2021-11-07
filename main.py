c17ccc9155132fd7cc7bc4f68ff3dfc3175c362f
Valery Efremov <valeriy131100@gmail.com>
2021-11-07 21:42:05 +0300

    Add image download to images folder

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
+import os
+from pathlib import Path
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 filename = 'hubble.jpeg'
+file_path = 'images'
+
+Path(file_path).mkdir(parents=True, exist_ok=True)
 
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(os.path.join(file_path, filename), 'wb') as file:
     file.write(response.content)
