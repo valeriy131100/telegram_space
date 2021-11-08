2b1d95a9099ee457392f51d6f324841541f8d34c
Valery Efremov <valeriy131100@gmail.com>
2021-11-08 19:17:27 +0300

    Add publishing image by telegram api

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 import telegram
 from dotenv import load_dotenv
 
@@ -7,4 +8,10 @@ if __name__ == '__main__':
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     bot = telegram.Bot(token=telegram_token)
-    bot.send_message(chat_id='@justsomespaceposting', text='Just bot test')
+
+    _, _, images = next(os.walk('images'))
+    image_filename = random.choice(images)
+    image_path = os.path.join('images', image_filename)
+    with open(image_path, 'rb') as image_file:
+        telegram_image = telegram.InputMediaPhoto(media=image_file)
+        bot.send_media_group(chat_id='@justsomespaceposting', media=[telegram_image])
