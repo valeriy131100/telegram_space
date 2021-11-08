db2c551319e70c99f7d09f9af3cacab2a908695e
Valery Efremov <valeriy131100@gmail.com>
2021-11-08 19:29:18 +0300

    Add infinite posting with configurable latency

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 import telegram
 from dotenv import load_dotenv
 
@@ -7,11 +8,20 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
+    channel_name = os.getenv('TELEGRAM_CHANNEL_NAME')
+    s_latency = os.getenv('TELEGRAM_POSTING_LATENCY')
+    if s_latency:
+        latency = int(s_latency)
+    else:
+        latency = 60*60*24  # сутки
     bot = telegram.Bot(token=telegram_token)
 
-    _, _, images = next(os.walk('images'))
-    image_filename = random.choice(images)
-    image_path = os.path.join('images', image_filename)
-    with open(image_path, 'rb') as image_file:
-        telegram_image = telegram.InputMediaPhoto(media=image_file)
-        bot.send_media_group(chat_id='@justsomespaceposting', media=[telegram_image])
+    while True:
+        _, _, images = next(os.walk('images'))
+        image_filename = random.choice(images)
+        image_path = os.path.join('images', image_filename)
+        with open(image_path, 'rb') as image_file:
+            telegram_image = telegram.InputMediaPhoto(media=image_file)
+            bot.send_media_group(chat_id=channel_name, media=[telegram_image])
+
+        time.sleep(latency)
