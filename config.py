cae777bd33ea91ec4d517487e12795574172c74a
Valery Efremov <valeriy131100@gmail.com>
2021-11-18 21:50:23 +0300

    Extract dir creating to separate function

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
+from file_workers import create_path_if_not_exists
 
 load_dotenv()
 
 images_folder = os.getenv('IMAGES_FOLDER', default='images')
+create_path_if_not_exists(images_folder)
 
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 telegram_channel_name = os.getenv('TELEGRAM_CHANNEL_NAME')
--- file_workers.py
+++ file_workers.py
@@ -11,12 +11,14 @@ def get_url_file_extension(url):
     return extension
 
 
+def create_path_if_not_exists(file_path):
+    file_dirname = os.path.dirname(file_path)
+    Path(file_dirname).mkdir(parents=True, exist_ok=True)
+
+
 def download_image(image_url, image_path, http_params=None):
     response = requests.get(image_url, params=http_params)
     response.raise_for_status()
-
-    image_dirname = os.path.dirname(image_path)
-    Path(image_dirname).mkdir(parents=True, exist_ok=True)
 
     with open(image_path, 'wb') as file:
         file.write(response.content)
