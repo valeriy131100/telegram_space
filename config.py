84d2092fa947483f63a6bfc8cae3c986394b5190
Valery Efremov <valeriy131100@gmail.com>
2021-11-22 21:40:21 +0300

    Remove unnecessary type conversion

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ images_folder = os.getenv('IMAGES_FOLDER', default='images')
 
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 telegram_channel_name = os.getenv('TELEGRAM_CHANNEL_NAME')
-s_latency = os.getenv('TELEGRAM_POSTING_LATENCY', default=str(60*60*24))
+s_latency = os.getenv('TELEGRAM_POSTING_LATENCY', default=60*60*24)
 latency = int(s_latency)
 
 nasa_token = os.getenv('NASA_TOKEN')
